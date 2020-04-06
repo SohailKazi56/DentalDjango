588c636fa1b61e9ea619a732792780a07dc7dcac
Sohail Kazi <sohailkazi56@gmail.com>
2020-04-07 00:54:29 +0530

    errorfour

--- dental/settings.py
+++ dental/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'w    hitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'dental.urls'
